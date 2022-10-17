7901c9e557bc94657d794f022566333cc8c2d517
ehemsy <116014216+ehemsy@users.noreply.github.com>
2022-10-17 11:22:07 -0500

    Update streamlit_app.py
    Set index to Fruit names

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3-us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list.set_index('Fruit', inplace = True)
 
 # Lets put a pick lit here so they can pick the fruit they want to include
 streamlit.multiselect('Pick some furits:', list(my_fruit_list.index))
